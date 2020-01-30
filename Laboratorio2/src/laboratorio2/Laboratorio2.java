b7ed66bc43b1bb96ac897a892991864e4b204eaf
LuisSosaR1999 <lsosarivas@gmail.com>
2020-01-30 10:28:28 -0600

    Otra prueba

---
+++ Laboratorio2/src/laboratorio2/Laboratorio2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio2;
+
+/**
+ *
+ * @author lsosa
+ */
+public class Laboratorio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nombre[][]=new String [3][0];
+        float f,c;
+        float sueldo[][]=new float[3][3];
+        float ISR[][]=new float[3][3];
+        float IGS[][]=new float[3][3];
+      
+        
+        System.out.println("Ingrese 3 nombres"+nombre);
+        for (int i=0;i<sueldo.length;i++){
+            for(int j=0;j<sueldo[0].length;j++){
+             sueldo[i][j]=(i*sueldo.length)+(j+1);
+             System.out.print(sueldo[i][j]+" ");
+             
+            }
+            System.out.println("");
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
